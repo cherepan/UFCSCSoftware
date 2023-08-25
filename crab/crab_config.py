4979696e0602ba24df7e54e822fca7efd04f4c8b
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2023-08-25 16:08:59 +0200

    update crab config; pset;

--- crab/crab_config.py
+++ crab/crab_config.py
@@ -2,26 +2,27 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'CSCLocalReco_26_05_2023'
+config.General.requestName = 'RAW2DIGI_L1Reco_RECO_RECOSIM'
 config.General.workArea =  'crab_area'
 config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'makeLocalReco.py'
+config.JobType.psetName = 'ProduceReco_v2_py_RAW2DIGI_L1Reco_RECO_RECOSIM.py'
+config.JobType.maxMemoryMB = 2500
 
 config.section_("Data")
 
-config.Data.inputDataset = '/SingleMuon/Run2018B-ZMu-12Nov2019_UL2018-v2/RAW-RECO'
+config.Data.inputDataset = '/RelValZMM_PU_13p6/CMSSW_12_4_12-PU_124X_mcRun3_2022_realistic_postEE_forPixelIneff_v5_PDMVRELVALS188_HS_2023PU-v1/GEN-SIM-DIGI-RAW'
 config.Data.inputDBS  = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 5
-config.Data.totalUnits  = 20
-config.Data.lumiMask = 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits  = 60
+#config.Data.lumiMask = 'Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'
 config.Data.publication = True
 config.Data.outLFNDirBase = '/store/user/cherepan'
-config.Data.outputDatasetTag = 'CSC_LocalReco_reProcess_UF_algo_Run2018B_ZMu_12Nov2019'
+config.Data.outputDatasetTag = 'RelValZMM_14_CMSSW_12_4_0RAW2DIGI_L1Reco_RECO_RECOSIM'
 
 config.section_("Site")
 #config.Site.whitelist = ['T2_US_Wisconsin','T2_US_Purdue','T1_US_FNAL']
